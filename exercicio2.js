75ec012926594f1e48bde9d30ca0a5835688bea4
Fabio Quintal <fabioamquintal@gmail.com>
2022-04-04 18:58:39 -0300

    fixed variable value

--- exercicio2.js
+++ exercicio2.js
@@ -3,13 +3,14 @@ const exercicioDois = (blackUniform, orangeUniform) => {
     const orangeObj = {}
     let countBlack=0;
     let countOrange=0;
+    const tamanho = blackUniform.length;
     //Utilizando objetos para aproveitar a ordenação no método Object.values()
-    for(let i=0; i<blackUniform.length; i++){
+    for(let i=0; i<tamanho; i++){
         blackObj[blackUniform[i]] = blackUniform[i]
         orangeObj[orangeUniform[i]] = orangeUniform[i]
     }
     //loop secundário para ter o resultado correto no caso de um array com ordenação crescente e outro array com ordenação decrescente
-    for(let i=0; i<blackUniform.length; i++){
+    for(let i=0; i<tamanho; i++){
         if(Object.values(blackObj)[i] < Object.values(orangeObj)[i]){
             countBlack++
         }
@@ -17,5 +18,5 @@ const exercicioDois = (blackUniform, orangeUniform) => {
             countOrange++
         }
     }
-    return Math.max(countBlack, countOrange) === blackUniform.length ? true : false
+    return Math.max(countBlack, countOrange) === tamanho ? true : false
 }
